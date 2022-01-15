38f5345635e1adde1f9531a1d6910d349e7c79f7
g0j <sergkuz92@gmail.com>
2022-01-15 11:18:59 +0300

    frtch bonds with post

--- src/services/bondService.js
+++ src/services/bondService.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 export function fetchBonds(token) {
-  return axios.get('https://coproxy.herokuapp.com/rest/tinkoff.public.invest.api.contract.v1.InstrumentsService/Bonds', {
+  return axios.post('https://coproxy.herokuapp.com/rest/tinkoff.public.invest.api.contract.v1.InstrumentsService/Bonds', 
+  {
+    "instrumentStatus": "INSTRUMENT_STATUS_UNSPECIFIED"
+  },
+  {
+    dataType: 'json',
     headers: {
       'Authorization': `Bearer ${token}`,
     },
